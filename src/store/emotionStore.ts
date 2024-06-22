d113e9e1de87494345f0233a372128fdc7e845f1
ghost75046 <ghost75046@yandex.ru>
2024-06-22 20:31:11 +0300

    Вернул звук мурчания при хорошем настроении, который потерялся при рефакторинге в ходе перехода на MobX

--- src/store/emotionStore.ts
+++ src/store/emotionStore.ts
@@ -5,17 +5,24 @@ import sad from "../images/emotionSad.png"
 import foodLevelStore from "./foodLevelStore";
 import kakaLevelStore from "./kakaLevelStore";
 import playingStore from "./playingStore";
+import Mrrr from "../audio/Mrrr.mp3";
 
 
 const emotionStore = observable({
     currentEmotion: normal,
+    mrrrSound: new Audio(Mrrr),
     changeEmotion() {
         if (foodLevelStore.foodLevel.length < 3 || kakaLevelStore.kakaLevel.length > 1 || !playingStore.ifPlayedRecently) {
             emotionStore.currentEmotion = sad
+            emotionStore.mrrrSound.pause()
         } else if (foodLevelStore.foodLevel.length > 3 && kakaLevelStore.kakaLevel.length === 0 && playingStore.ifPlayedRecently) {
             emotionStore.currentEmotion = happy
+            emotionStore.mrrrSound.play()
+
         } else {
             emotionStore.currentEmotion = normal
+            emotionStore.mrrrSound.pause()
+            ///
         }
     }
 
